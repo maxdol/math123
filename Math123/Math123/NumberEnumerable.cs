f4d0c13bf0689ad6ced732ea8fde9e81d978a788
Max Dolidze <maxim.dolidze@kdi.kongsberg.com>
2017-04-16 22:59:57 +0300

    Add NumbrEnumerable class

---
+++ Math123/Math123/NumberEnumerable.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Math123
+{
+    public static class NumberEnumerable
+    {
+        public static IEnumerable<long> D0 => Enumerable.Range(0, 1).Select(i => (long)i);
+        public static IEnumerable<long> D1 => Enumerable.Range(1, 1).Select(i => (long)i);
+        public static IEnumerable<long> D2 => Enumerable.Range(2, 1).Select(i => (long)i);
+        public static IEnumerable<long> D3 => Enumerable.Range(3, 1).Select(i => (long)i);
+        public static IEnumerable<long> D4 => Enumerable.Range(4, 1).Select(i => (long)i);
+        public static IEnumerable<long> D5 => Enumerable.Range(5, 1).Select(i => (long)i);
+        public static IEnumerable<long> D6 => Enumerable.Range(6, 1).Select(i => (long)i);
+        public static IEnumerable<long> D7 => Enumerable.Range(7, 1).Select(i => (long)i);
+        public static IEnumerable<long> D8 => Enumerable.Range(8, 1).Select(i => (long)i);
+        public static IEnumerable<long> D9 => Enumerable.Range(9, 1).Select(i => (long)i);
+
+
+        public static IEnumerable<long> Digits => Enumerable.Range(0, 10).Select(i => (long)i);
+        public static IEnumerable<long> NonZeroDigits => Enumerable.Range(1, 9).Select(i => (long)i);
+
+        public static IEnumerable<long> MakeNumber(IEnumerable<IEnumerable<long>> digits)
+        {
+            var d = digits.ToList();
+            var count = d.Count;
+
+            return MakeNumberReq(d.Select((r, i) => new Tuple<long, IEnumerable<long>>(TenPow(count - i - 1), r)));
+        }
+
+        public static long TenPow(int i)
+        {
+            if (i == 0)
+                return 1;
+
+            return Enumerable.Range(1, i).Select(x => 10).Aggregate(1, (res, x) => res * x);
+        }
+
+
+        private static IEnumerable<long> MakeNumberReq(IEnumerable<Tuple<long, IEnumerable<long>>> digits)
+        {
+            var first = digits.First();
+            var rest = digits.Skip(1);
+            if (rest.Any())
+            {
+                foreach (var d1 in first.Item2)
+                {
+                    foreach (var restPart in MakeNumberReq(rest))
+                    {
+                        yield return first.Item1 * d1 + restPart;
+                    }
+                }
+            }
+            else
+            {
+                foreach (var i in first.Item2)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
---
+++ Math123/Math123Tests/NumberEnumerableTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using N = Math123.NumberEnumerable;
+
+namespace Math123Tests
+{
+    [TestFixture]
+    public class NumberEnumerableTests
+    {
+        [Test]
+        public void TestTenPow()
+        {
+            Assert.AreEqual(1, N.TenPow(0));
+            Assert.AreEqual(10, N.TenPow(1));
+            Assert.AreEqual(100, N.TenPow(2));
+            Assert.AreEqual(1000, N.TenPow(3));
+            Assert.AreEqual(1000000, N.TenPow(6));
+        }
+
+        [Test]
+        public void TestMakeNumer()
+        {
+            var numbers = N.MakeNumber(new[] { N.Digits, N.Digits, N.Digits }).ToList();
+
+            Assert.AreEqual(1000, numbers.Count());
+            for (int i = 0; i != 1000; ++i)
+            {
+                Assert.AreEqual(i, numbers[i]);
+            }
+        }
+    }
+}
