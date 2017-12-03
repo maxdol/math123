21796d523285b21f77d22cd2ea50ba80ecc57a0e
Maxim Dolidze <maxim.dolidze@gmail.com>
2017-12-04 01:39:02 +0300

    add training app proj

---
+++ Math123/TrainingTestApp/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TrainingTestApp
+{
+    public class Command : ICommand
+    {
+        private Action<object> commandAction; 
+        public Command(Action<object> commandAction)
+        {
+            this.commandAction = commandAction;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            commandAction(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+
+    public class MainWindowViewModel
+    {
+        public ICommand PrimeNumbers { get { return new Command(o=> MessageBox.Show("Простые числа")); } }
+    }
+}
